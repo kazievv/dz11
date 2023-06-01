6703689c8f03fb1c2b17d11a69386c0d8b3c5786
Kaziev <kazie_v15@mail.ru>
2023-06-01 19:18:42 +0300

    y

--- dz11/Program.cs
+++ dz11/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA = ReadInt("Введите число A: ");
+int numberB = ReadInt("Введите число B: ");
+ToDegree(numberA, numberB);
+
+// Функция возведения в степень
+void ToDegree(int a, int b)
+{
+int result = 1;
+for (int i = 1; i <= b; i++)
+{
+result = result * a;
+}
+Console.WriteLine(result);
+}
+
+// Функция ввода
+int ReadInt(string message)
+{
+Console.WriteLine(message);
+return Convert.ToInt32(Console.ReadLine());
+}
